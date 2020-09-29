42be422580ba78348fb6e210949cba95b6de9833
DesignofCp <4IN1Telecommunications@gmail.com>
2020-09-29 16:41:41 -0500

    Add files via upload

---
+++ MFZ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MFZ
+//
+//  Created by Charlithia Palmer on 8/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
